43b1baa2d0839472a34edf299dcd240b9b522e4f
aftab35520 <aftab.alam35520@gmail.com>
2026-08-05 17:44:29 +0530

    first

--- Agent/state.py
+++ Agent/state.py
@@ -1,14 +1,23 @@
-from typing import TypedDict,Annotated,Optional,Literal
-from langgraph.graph.message import add_messages
+from typing import Annotated, Literal, TypedDict
+from langchain_core.messages import BaseMessage
+
+
+def keep_last_5_messages(
+    left: list[BaseMessage],
+    right: list[BaseMessage] | BaseMessage,
+) -> list[BaseMessage]:
+    if not isinstance(right, list):
+        right = [right]
+
+    return (left + right)[-5:]
+
 
 class AgentState(TypedDict):
-    messages:Annotated[list,add_messages]
+    messages: Annotated[list[BaseMessage], keep_last_5_messages]
     plan: list[str]
-    task_to_execute:str
-    Completed_task:list[str]
-    workflow_status:Literal["RUNNING", "COMPLETED"]
-    Intrupt:Literal["YES","NO"]
-    Intrupt_Question:str
-    connection_id:str
- 
- 
+    task_to_execute: str
+    Completed_task: list[str]
+    workflow_status: Literal["RUNNING", "COMPLETED"]
+    Intrupt: Literal["YES", "NO"]
+    Intrupt_Question: str
+    connection_id: str
